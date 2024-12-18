94a0f0fefcb443e4007f207f6593876c29969f6c
stssmlr <stanislav.smolyar@gmail.com>
2024-12-18 20:13:11 +0200

    page nav

--- my-react/src/App.tsx
+++ my-react/src/App.tsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import Card from "./components/common/card";
+import {useEffect} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    console.log("Use Effect!");
+}, []);
+
+console.log("Render component :)");
 
   return (
     <>
-      <h1>hello react</h1>
+      <button type="button"
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default
+            </button>
+            <Card/>
+            <h1 className="text-center text-3xl font-bold underline">
+                Hello world!
+            </h1>
     </>
   )
 }
